fda8fda87d79fee4236c5d84a99d22e1e4676761
kiettt23 <kiettt23@gmail.com>
2025-09-18 18:13:26 +0700

    fix: update layout to ensure Footer sticks to the bottom

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,12 +4,14 @@ import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+      {/* Đặt flex column và min-height cho toàn bộ trang */}
       <Navbar />
-      <main>
+      <main className="flex-grow">
+        {/* Chạy flex-grow để đảm bảo phần này chiếm hết không gian còn lại */}
         <Outlet />
       </main>
-      <Footer />
+      <Footer /> {/* Footer sẽ ở dưới cùng nhờ flexbox */}
     </div>
   );
 };
